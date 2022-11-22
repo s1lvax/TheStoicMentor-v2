6e0f00178152a07fc8c733d39f687c12b29dddfd
csilva49 <silva.cesario49@gmail.com>
2022-11-22 11:47:49 +0000

    Bot sending all Journal entries

--- commands/myjournal.js
+++ commands/myjournal.js
@@ -6,17 +6,20 @@ module.exports = {
       .setName('myjournal')
       .setDescription('Retrieve all your journal entries!'),
    async execute(interaction) {
-      const userID = Number(interaction.user.id);
+      //const userID = Number(interaction.user.id);
 
-      Schema.find({}, function (err, docs) {
-         docs.forEach(element => console.log(element['message'] + '\n'));
+      Schema.find({ author: 225633052645261313 }, function (err, docs) {
+         const messages = [];
+         docs.forEach(element => messages.push(element['message']));
+
+         const messagesString = messages.join('\r\n\n');
+
+         if (!messages) {
+            interaction.reply({ content: "You have 0 journal entries.", ephemeral: true });
+         }
+         if (messages) {
+            interaction.reply({ content: messagesString, ephemeral: true });
+         }
       });
-      console.log(userData);
-      if (!userData) {
-         await interaction.reply({ content: "You have 0 journal entries.", ephemeral: true });
-      }
-      if (userData) {
-         await interaction.reply({ content: "I found entries", ephemeral: true });
-      }
    },
 };
